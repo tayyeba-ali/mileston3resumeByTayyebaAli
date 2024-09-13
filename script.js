a66fb7ec67a4f3413d07aeadc9c1fc12c2813adc
Tayyeba Ali <155741204+tayyeba-ali@users.noreply.github.com>
2024-09-14 04:42:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 var _a;
 //listing element
 (_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    var _a;
     event.preventDefault();
+    //Get reference to form elements using their IDs:
+    var profilePictureInput = document.getElementById('profilePicture');
     ///type assertion
     var nameElement = document.getElementById('name');
     var emailElement = document.getElementById('email');
     var phoneElement = document.getElementById('phone');
+    var addressElement = document.getElementById('address');
     var educationElement = document.getElementById('education');
     var experienceElement = document.getElementById('experience');
     var skillsElement = document.getElementById('skills');
-    if (nameElement && emailElement && phoneElement && educationElement && experienceElement && skillsElement) {
+    if (profilePictureInput && nameElement && emailElement && phoneElement && addressElement && educationElement && experienceElement && skillsElement) {
         var name_1 = nameElement.value;
         var email = emailElement.value;
         var phone = phoneElement.value;
+        var address = addressElement.value;
         var education = educationElement.value;
         var experience = experienceElement.value;
         var skill = skillsElement.value;
+        //picture elements
+        var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
+        var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
         //Create resume output
-        var resumeOutput = "\n    <h2>Resume</h2>\n    <p><strong>Name :</strong> ".concat(name_1, " </p>\n    <p><strong>Email :</strong> ").concat(email, " </p>\n    <p><strong>Phone Number :</strong> ").concat(phone, " </p>\n    \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skill, "</p>\n    ");
+        var resumeOutput = "\n   <h2>Resume</h2>\n   ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n   <p><strong>Name :</strong> ").concat(name_1, " </p>\n    <p><strong>Email :</strong> ").concat(email, " </p>\n    <p><strong>Phone Number :</strong> ").concat(phone, " </p>\n   \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skill, "</p>\n    ");
+        //Display the resume output
         var resumeOutputElement = document.getElementById('resumeOutput');
         if (resumeOutputElement) {
             resumeOutputElement.innerHTML = resumeOutput;
