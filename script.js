6c94d315002282dcec8b72952d8199700169789b
Tayyeba Ali <155741204+tayyeba-ali@users.noreply.github.com>
2024-09-14 04:51:43 +0500

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var _a;
         var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
         var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
         //Create resume output
-        var resumeOutput = "\n   <h2>Resume</h2>\n   ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n   <p><strong>Name :</strong> ").concat(name_1, " </p>\n    <p><strong>Email :</strong> ").concat(email, " </p>\n    <p><strong>Phone Number :</strong> ").concat(phone, " </p>\n   \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skill, "</p>\n    ");
+        var resumeOutput = "\n   <h2>Resume</h2>\n   ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : '', "\n   <p><strong>Name :</strong> ").concat(name_1, " </p>\n    <p><strong>Email :</strong> ").concat(email, " </p>\n    <p><strong>Phone Number :</strong> ").concat(phone, " </p>\n    <p><strong>Address :</strong> ").concat(address, " </p>\n\n   \n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skill, "</p>\n    ");
         //Display the resume output
         var resumeOutputElement = document.getElementById('resumeOutput');
         if (resumeOutputElement) {
